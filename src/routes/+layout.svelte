d63e8aba159b5f68713210c0c630595477e50f06
Sergio Segrera <sergioarturosegrera@gmail.com>
2023-01-01 18:00:23 -0500

    Add google analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,20 @@
 	import Bottom from '../lib/bottom.svelte';
 </script>
 
+<svelte:head>
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-Y93WTD4WF5"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'G-Y93WTD4WF5');
+	</script>
+</svelte:head>
+
 <Top />
 <slot />
 <Bottom />
